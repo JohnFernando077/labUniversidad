fc83bb6c19b49d80ef615ecef015fb81f24b8220
JohnFernando077 <jfcalle07@gmail.com>
2024-11-08 14:23:12 -0500

    add estudiante.py

---
+++ clases/estudiante.py
@@ -0,0 +1,16 @@
+from persona import Persona
+
+
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, sexo, carnet, carrera):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.carnet = carnet
+        self.carrera = carrera
+     
+    def __str__(self):
+        return f"El estudiante {self.nombre} tiene {self.edad} años de edad y su sexo es {self.sexo} su carnet es de {self.carnet} y su carrera es de {self.carrera}"
+
+persona1 = Estudiante("John Calle", 28, "hombre", "primer ciclo", "sistemas")
+print(persona1)
