b690bf06f309de926e7eac4a323b742c232b6c40
JohnFernando077 <jfcalle07@gmail.com>
2024-11-08 15:15:20 -0500

    estudiante.py

--- clases/estudiante.py
+++ clases/estudiante.py
@@ -12,5 +12,5 @@ class Estudiante(Persona):
     def __str__(self):
         return f"El estudiante {self.nombre} tiene {self.edad} años de edad y su sexo es {self.sexo} su carnet es de {self.carnet} y su carrera es de {self.carrera}"
 
-persona1 = Estudiante("John Calle", 28, "hombre", "primer ciclo", "sistemas")
-print(persona1)
+#persona1 = Estudiante("John Calle", 28, "hombre", "primer ciclo", "sistemas")
+#print(persona1)
