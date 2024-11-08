5c87ee04b0a9e05b4a3abe8d17997ac94f2e30c9
JohnFernando077 <jfcalle07@gmail.com>
2024-11-08 14:16:05 -0500

    add persona.py

--- clases/persona.py
+++ clases/persona.py
@@ -4,8 +4,14 @@ class Persona:
         self.edad = edad
         self.sexo = sexo
     
+    def __str__(self):
+        return f"{self.nombre} de {self.edad} años de edad, de sexo {self.sexo}"
+    
     def correr(self):
         return f"Usted {self.nombre} tiene {self.edad} años de edad, su sexo es {self.sexo} y está corriendo."
+
+persona0 = Persona("Juan", 20, "Hombre")
+print(persona0)
 persona1 = Persona("John Calle", 28, "masculino")
 print(persona1.correr())
  
