94459673e4331ffeb55e7f02910cae5ee2a645ed
JohnFernando077 <jfcalle07@gmail.com>
2024-11-08 14:32:12 -0500

    add profesor.py

---
+++ clases/profesor.py
@@ -0,0 +1,16 @@
+from persona import Persona
+
+class Profesor(Persona):
+    def __init__(self, nombre, edad,sexo, codigo, especialidad):
+        self.nombre = nombre
+        self.edad = edad
+        self.sexo = sexo
+        self.codigo = codigo
+        self.especialidad = especialidad
+
+    def __str__(self):
+        return f"El docente {self.nombre} tiene {self.edad} años de edad, su sexo es {self.sexo}, su codigo es {self.codigo} y su especialidad es {self.especialidad}"
+profesor1 = Profesor("John Calle", 28, "hombre", "ABC123", "Sistemas")
+print(profesor1)
+        
+        
