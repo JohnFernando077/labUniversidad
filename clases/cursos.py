eded0093e95bdb9675753e6f89e58de6e970ee80
JohnFernando077 <jfcalle07@gmail.com>
2024-11-08 22:17:58 -0500

    add cursos.py

--- clases/cursos.py
+++ clases/cursos.py
@@ -1,13 +1,12 @@
-from profesor import Profesor
+from .profesor import Profesor
 
-class Cursos(Profesor):
-    def __init__(self, curso, codigo, nombre):
-        self.curso = curso
-        self.codigo = codigo
+class Cursos:
+    def __init__(self, nombre, codigo, profesor):
         self.nombre = nombre
+        self.codigo = codigo
+        self.profesor = profesor  # Aquí, `profesor` es un objeto de la clase `Profesor`
         
-    def __str__(self):
-        return f"El curso de {self.curso} con codigo {self.codigo} sera dictado por el docente {self.nombre}"
 
-#curso1 = Cursos("Pimer ciclo", "ABC123", "John Calle")
-#print(curso1)
+    def __str__(self):
+        return (f"Curso: {self.nombre}, Código: {self.codigo}, "
+                f"Profesor: {self.profesor.nombre} - {self.profesor.especialidad}")
