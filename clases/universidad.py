33833742b5633d94dbd55b7046ff9f5e058e3375
JohnFernando077 <jfcalle07@gmail.com>
2024-11-08 22:20:43 -0500

    universidad.py

--- clases/universidad.py
+++ clases/universidad.py
@@ -1,13 +1,18 @@
+#from .cursos import Cursos
+
 class Universidad:
-    def __init__(self, nombre, cursos):
+    def __init__(self, nombre, cursos=None):
         self.nombre = nombre
-        self.cursos = cursos if cursos else []    
+        self.cursos = cursos if cursos is not None else []    
 
     def agregar_curso(self, curso):
+        #metodo para agregar cursos en la universidad
         self.cursos.append(curso)
     
     def __str__(self):
-        return  f"La universidad {self.nombre} tiene los cursos de {self.cursos}"
+        #return  f"La universidad {self.nombre} tiene los cursos de {self.cursos}"
+        cursos_str = "\n".join([str(curso) for curso in self.cursos]) 
+        return f"Universidad: {self.nombre}\nCursos:\n{cursos_str if cursos_str else "no hay cursos disponibles"}"
 
 #universidad1 = Universidad("SPOCH", ["1mer ciclo", "2do ciclo", "3er ciclo"])
 #print(universidad1)    
